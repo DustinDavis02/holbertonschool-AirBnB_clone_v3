376b036a7e043897ca1320e7ea76657630a76f44
Dustin Davis <davisdustin328@gmail.com>
2023-03-30 10:58:34 -0500

    Recieves object with 2 parameters. Using SQL ORM Return object

--- models/engine/db_storage.py
+++ models/engine/db_storage.py
@@ -74,3 +74,11 @@ class DBStorage:
     def close(self):
         """call remove() method on the private session attribute"""
         self.__session.remove()
+
+    def get(self, cls, id):
+        """Retrieve an object from the database"""
+        if cls and id:
+            obj = self.__session.query(cls).filter_by(id=id).first()
+            return obj
+        return None
+
