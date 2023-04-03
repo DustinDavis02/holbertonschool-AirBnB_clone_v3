b1c13588a4788390e9d3dc4cb18741288238db3b
Dustin Davis <davisdustin328@gmail.com>
2023-04-03 17:35:30 -0500

    Return with the number of object in storage by type

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -1,7 +1,15 @@
 #!/usr/bin/python3
 from flask import jsonify
 from api.v1.views import app_views
+from models import storage
 
 @app_views.route('/status')
 def status():
     return jsonify({'status': 'OK'})
+
+@app_views.route('/stats')
+def stats():
+    stats = {}
+    for cls in storage.all().keys():
+        stats[cls] = storage.count(cls)
+    return jsonify(stats)
