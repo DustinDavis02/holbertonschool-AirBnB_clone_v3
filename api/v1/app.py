2043f251aa41553a41e89222d5590cc0b3f0043a
Dustin Davis <davisdustin328@gmail.com>
2023-04-03 17:40:12 -0500

    Adding documentation and making it PEP8 approved

--- api/v1/app.py
+++ api/v1/app.py
@@ -1,3 +1,5 @@
+#!/usr/bin/python3
+"""Main module to start the API"""
 from flask import Flask
 from models import storage
 from api.v1.views import app_views
@@ -8,10 +10,10 @@ app.register_blueprint(app_views, url_prefix='/api/v1')
 
 @app.teardown_appcontext
 def teardown_storage(exception):
+    """Remove the current SQLAlchemy Session"""
     storage.close()
 
 if __name__ == "__main__":
     host = os.getenv('HBNB_API_HOST', '0.0.0.0')
     port = int(os.getenv('HBNB_API_PORT', 5000))
     app.run(host=host, port=port, threaded=True)
-
--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -1,14 +1,17 @@
 #!/usr/bin/python3
+"""Index module to create an API endpoint"""
 from flask import jsonify
 from api.v1.views import app_views
 from models import storage
 
-@app_views.route('/status')
+@app_views.route('/status', methods=['GET'], strict_slashes=False)
 def status():
+    """Returns a JSON with the status of the API"""
     return jsonify({'status': 'OK'})
 
-@app_views.route('/stats')
+@app_views.route('/stats', methods=['GET'], strict_slashes=False)
 def stats():
+    """Returns a JSON with the number of objects in storage by type"""
     stats = {}
     for cls in storage.all().keys():
         stats[cls] = storage.count(cls)
