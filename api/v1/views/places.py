361b88e5d4e9ed7378fa3079c0e2af3266bd1f27
Dustin Davis <davisdustin328@gmail.com>
2023-04-04 18:36:24 -0500

    Retrieves list of all Place objects of City with given city_id

--- api/v1/views/places.py
+++ api/v1/views/places.py
@@ -4,3 +4,19 @@
 from flask import jsonify, abort, request, make_response
 from api.v1.views import app_views
 from models import storage, Place, City, User
+
+
+@app_views.route('/cities/<city_id>/places', methods=['GET'], strict_slashes=False)
+def get_places(city_id):
+    """
+    Retrieves the list of all Place objects of a City
+    """
+    city = storage.get(City, city_id)
+    if city is None:
+        abort(404)
+
+    places_list = []
+    places = city.places
+    for place in places:
+        places_list.append(place.to_dict())
+    return jsonify(places_list)
