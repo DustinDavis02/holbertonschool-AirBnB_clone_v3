becbda6f724ddbbc1e021cc5cbac88765fa509c8
Dustin Davis <davisdustin328@gmail.com>
2023-04-04 16:35:11 -0500

    Gathers list of all Amenity objects

--- api/v1/amenities.py
+++ api/v1/amenities.py
@@ -5,3 +5,11 @@ from api.v1.views import app_views
 from flask import jsonify, abort, request
 from models import storage
 from models.amenity import Amenity
+
+
+@app_views.route('/amenities', methods=['GET'], strict_slashes=False)
+def get_amenities():
+    """Retrieves the list of all Amenity objects"""
+    amenities = storage.all(Amenity).values()
+    amenities_list = [amenity.to_dict() for amenity in amenities]
+    return jsonify(amenities_list)
