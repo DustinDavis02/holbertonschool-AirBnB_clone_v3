c8954abb2e448b664d638fd9fbc2df70ce7469f5
Dustin Davis <davisdustin328@gmail.com>
2023-04-03 21:03:43 -0500

    Handles the JSON 404 Error

--- api/v1/app.py
+++ api/v1/app.py
@@ -1,17 +1,22 @@
 #!/usr/bin/python3
 """Main module to start the API"""
-from flask import Flask
+from flask import Flask, jsonify
 from models import storage
 from api.v1.views import app_views
 import os
 
 app = Flask(__name__)
-app.register_blueprint(app_views, url_prefix='/api/v1')
+app.register_blueprint(app_views)
 
 @app.teardown_appcontext
-def teardown_storage(exception):
+def close_storage(error):
     """Remove the current SQLAlchemy Session"""
     storage.close()
+    
+@app.errorhandler(404)
+def not_found(error):
+    """Handler for 404 errors that returns a JSON-formatted 404 status code response"""
+    return jsonify({"error": "Not found"}), 404
 
 if __name__ == "__main__":
     host = os.getenv('HBNB_API_HOST', '0.0.0.0')
