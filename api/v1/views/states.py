a02579fa621633647363478cf4bf07393cb756ed
Dustin Davis <davisdustin328@gmail.com>
2023-04-03 21:16:00 -0500

    Gets list of all states through models.states

--- api/v1/views/states.py
+++ api/v1/views/states.py
@@ -5,3 +5,13 @@ from api.v1.views import app_views
 from models import storage
 from models.state import State
 from flask import jsonify, abort, request
+
+
+@app_views.route('/states', methods=['GET'], strict_slashes=False)
+def get_states():
+    """Retrieves the list of all State objects"""
+    states = storage.all(State).values()
+    state_list = []
+    for state in states:
+        state_list.append(state.to_dict())
+    return jsonify(state_list)
