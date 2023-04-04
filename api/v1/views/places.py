4f7ab4bc7219f098a87979737897b818b1f46ff0
Dustin Davis <davisdustin328@gmail.com>
2023-04-04 18:39:57 -0500

    Retrieves a Place object with the given place_id

--- api/v1/views/places.py
+++ api/v1/views/places.py
@@ -8,9 +8,7 @@ from models import storage, Place, City, User
 
 @app_views.route('/cities/<city_id>/places', methods=['GET'], strict_slashes=False)
 def get_places(city_id):
-    """
-    Retrieves the list of all Place objects of a City
-    """
+    """Retrieves the list of all Place objects of a City"""
     city = storage.get(City, city_id)
     if city is None:
         abort(404)
@@ -20,3 +18,12 @@ def get_places(city_id):
     for place in places:
         places_list.append(place.to_dict())
     return jsonify(places_list)
+
+
+@app_views.route('/places/<place_id>', methods=['GET'], strict_slashes=False)
+def get_place(place_id):
+    """Retrieves a Place object"""
+    place = storage.get(Place, place_id)
+    if place is None:
+        abort(404)
+    return jsonify(place.to_dict())
