25f2a9f523fa42b3fe7a571fcb1a1688f10a02f7
Dustin Davis <davisdustin328@gmail.com>
2023-04-04 16:23:45 -0500

    Delete a City object

--- api/v1/views/cities.py
+++ api/v1/views/cities.py
@@ -24,3 +24,14 @@ def get_city(city_id):
     if city is None:
         abort(404)
     return jsonify(city.to_dict())
+
+
+@app_views.route('/cities/<city_id>', methods=['DELETE'])
+def delete_city(city_id):
+    """Deletes a City object"""
+    city = storage.get(City, city_id)
+    if city is None:
+        abort(404)
+    storage.delete(city)
+    storage.save()
+    return make_response(jsonify({}), 200)
