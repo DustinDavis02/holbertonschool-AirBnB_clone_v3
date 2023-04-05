4153e72c374129fc3885152ccb3325c119f1636c
Dustin Davis <davisdustin328@gmail.com>
2023-04-04 19:41:23 -0500

    Retrieves the list of all Review objects of Place

--- api/v1/views/places_reviews.py
+++ api/v1/views/places_reviews.py
@@ -6,3 +6,15 @@ from models import storage
 from models.review import Review
 from models.place import Place
 from models.user import User
+
+
+@app_views.route('/places/<place_id>/reviews', methods=['GET'])
+def get_reviews(place_id):
+    """
+    Retrieves the list of all Review objects of a Place
+    """
+    place = storage.get("Place", place_id)
+    if place is None:
+        abort(404)
+    reviews_list = [review.to_dict() for review in place.reviews]
+    return jsonify(reviews_list)
