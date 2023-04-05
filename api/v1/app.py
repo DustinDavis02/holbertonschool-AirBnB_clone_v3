751d2c0ee5011b72b43f9a034a6f4574cc2df2bf
Dustin Davis <davisdustin328@gmail.com>
2023-04-04 19:47:18 -0500

    Create CORS instance

--- api/v1/app.py
+++ api/v1/app.py
@@ -1,24 +1,32 @@
 #!/usr/bin/python3
-"""Main module to start the API"""
-from flask import Flask, jsonify
-from models import storage
+""" API entry point """
+
+from os import getenv
+from flask import Flask, jsonify, make_response
+from flask_cors import CORS
 from api.v1.views import app_views
-import os
+from models import storage
 
 app = Flask(__name__)
 app.register_blueprint(app_views)
 
+cors = CORS(app, resources={r"/*": {"origins": "0.0.0.0"}})
+
+
 @app.teardown_appcontext
-def close_storage(error):
-    """Remove the current SQLAlchemy Session"""
+def teardown_storage(exception):
+    """ Tear down method """
     storage.close()
-    
+
+
 @app.errorhandler(404)
-def not_found(error):
-    """Handler for 404 errors that returns a JSON-formatted 404 status code response"""
-    return jsonify({"error": "Not found"}), 404
+def page_not_found(error):
+    """ 404 Error handler """
+    return make_response(jsonify({'error': 'Not found'}), 404)
+
 
 if __name__ == "__main__":
-    host = os.getenv('HBNB_API_HOST', '0.0.0.0')
-    port = int(os.getenv('HBNB_API_PORT', 5000))
+    host = getenv("HBNB_API_HOST", "0.0.0.0")
+    port = getenv("HBNB_API_PORT", 5000)
     app.run(host=host, port=port, threaded=True)
+
