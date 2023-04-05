936f833597f70c3d9166b6431a4141221938c4fa
WoodsonTD <taylordwoodson@gmail.com>
2023-04-04 21:58:09 -0500

    add

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python3
 """Index module to create an API endpoint"""
 from flask import jsonify
+from json import dumps
+from flask import Response
 from api.v1.views import app_views
 from models import storage
 
@@ -8,7 +10,7 @@ from models import storage
 def status():
     """Returns a JSON with the status of the API"""
     return jsonify({'status': 'OK'})
-
+    
 @app_views.route('/stats', methods=['GET'], strict_slashes=False)
 def stats():
     """Returns a JSON with the number of objects in storage by type"""
