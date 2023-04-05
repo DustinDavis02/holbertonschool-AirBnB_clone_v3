666c3a74c0360f49f3c8d4ba5f595eac9dbd9b66
Dustin Davis <davisdustin328@gmail.com>
2023-04-04 19:44:30 -0500

    Create and update review in database

--- api/v1/views/places_reviews.py
+++ api/v1/views/places_reviews.py
@@ -36,3 +36,43 @@ def delete_review(review_id):
     storage.delete(review)
     storage.save()
     return jsonify({}), 200
+
+
+@app_views.route('/places/<place_id>/reviews', methods=['POST'])
+def create_review(place_id):
+    """Creates a Review object"""
+    place = storage.get("Place", place_id)
+    if place is None:
+        abort(404)
+    data = request.get_json()
+    if data is None:
+        return jsonify(error="Not a JSON"), 400
+    if "user_id" not in data:
+        return jsonify(error="Missing user_id"), 400
+    user = storage.get("User", data["user_id"])
+    if user is None:
+        abort(404)
+    if "text" not in data:
+        return jsonify(error="Missing text"), 400
+    review = Review(**data)
+    review.place_id = place_id
+    storage.new(review)
+    storage.save()
+    return jsonify(review.to_dict()), 201
+
+
+@app_views.route('/reviews/<review_id>', methods=['PUT'])
+def update_review(review_id):
+    """Updates a Review object"""
+    review = storage.get("Review", review_id)
+    if review is None:
+        abort(404)
+    data = request.get_json()
+    if data is None:
+        return jsonify(error="Not a JSON"), 400
+    ignore_keys = ["id", "user_id", "place_id", "created_at", "updated_at"]
+    for key, value in data.items():
+        if key not in ignore_keys:
+            setattr(review, key, value)
+    storage.save()
+    return jsonify(review.to_dict()), 200
