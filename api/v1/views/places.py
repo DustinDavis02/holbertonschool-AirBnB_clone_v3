406cd5ab40fcda7de8f6e38199ca689bc11827af
Dustin Davis <davisdustin328@gmail.com>
2023-04-05 07:20:43 -0500

    Shrinking functions to make easier

--- api/v1/views/places.py
+++ api/v1/views/places.py
@@ -1,64 +1,67 @@
 #!/usr/bin/python3
 """This module contains a route that retrieves all places objects"""
 
-from api.v1.views import app_views, validate_model, get_json
-from flask import jsonify
+from api.v1.views import app_views
+from flask import jsonify, request, abort, make_response
 from models import storage
 from models.place import Place
 from models.city import City
 from models.user import User
 
 
-@app_views.route('/cities/<city_id>/places',
-                 methods=['GET'], strict_slashes=False)
-def get_all_places(city_id):
-    """Retrieves the list of all Place objects of a City"""
-    city = validate_model("City", city_id)
-    places = [place.to_dict() for place in city.places]
-    return jsonify(places)
+@app_views.route('/cities/<city_id>/places', methods=[
+    'GET'], strict_slashes=False)
+def get_places(city_id=None):
+    """retrieves all places associated with city id"""
+    city = storage.get(City, city_id)
+    if request.method == 'GET':
+        if city is None:
+            abort(404)
+        return jsonify([places.to_dict() for places in city.places])
 
 
-@app_views.route('/places/<place_id>',
-                 methods=['GET'], strict_slashes=False)
-def get_place(place_id):
-    """Retrieves a Place object"""
-    place = validate_model("Place", place_id)
-    return jsonify(place.to_dict())
+@app_views.route('/places/<place_id>', methods=['GET', 'DELETE', 'PUT'],
+                 strict_slashes=False)
+def place_get_or_delete(place_id=None):
+    """Retrieves a place or deletes a place"""
+    place = storage.get(Place, place_id)
+    if place is None:
+        abort(404)
+    if request.method == 'GET':
+        return jsonify(place.to_dict())
+    elif request.method == 'DELETE':
+        storage.delete(place)
+        storage.save()
+        return make_response(jsonify({}), 200)
+    elif request.method == 'PUT':
+        if request.is_json:
+            place_data = request.get_json()
+        else:
+            return make_response(jsonify({'error': 'Not a JSON'}), 400)
+        ignore_list = ['id', 'created_at', 'updated_at', 'city_id']
+        for key, val in place_data.items():
+            if key not in ignore_list:
+                setattr(place, key, val)
+        storage.save()
+        return make_response(jsonify(place.to_dict()), 200)
 
 
-@app_views.route('/places/<place_id>',
-                 methods=['DELETE'], strict_slashes=False)
-def delete_place(place_id):
-    """Deletes a Place object"""
-    place = validate_model("Place", place_id)
-    place.delete()
+@app_views.route('/cities/<city_id>/places', methods=['POST'],
+                 strict_slashes=False)
+def place_post(city_id=None):
+    """creates a place"""
+    place_data = request.get_json()
+    city = storage.get(City, city_id)
+    if city is None:
+        abort(404)
+    if not place_data:
+        return make_response(jsonify({'error': 'Not a JSON'}), 400)
+    if 'user_id' not in place_data.keys():
+        return make_response(jsonify({'error': 'Missing user_id'}), 400)
+    if storage.get(User, place_data['user_id']) is None:
+        abort(404)
+    if 'name' not in place_data.keys():
+        return make_response(jsonify({'error': 'Missing name'}), 400)
+    new_place = Place(**place_data)
     storage.save()
-    return jsonify({}), 200
-
-
-@app_views.route('/cities/<city_id>/places',
-                 methods=['POST'], strict_slashes=False)
-def place_no_id_post(city_id):
-    """Creates a Place"""
-    city = validate_model("City", city_id)
-    req_json = get_json(['user_id', 'name'])
-    user_id = req_json.get('user_id')
-    user = validate_model("User", user_id)
-    req_json['city_id'] = city_id
-    new_object = Place(**req_json)
-    new_object.save()
-    return jsonify(new_object.to_dict()), 201
-
-
-@app_views.route('/places/<place_id>',
-                 methods=['PUT'], strict_slashes=False)
-def place_with_id_put(place_id):
-    """Updates a Place object"""
-    place_obj = validate_model("Place", place_id)
-    req_json = get_json()
-    ignore_keys = ["id", "user_id", "city_id", "created_at", "updated_at"]
-    for key, value in req_json.items():
-        if key not in ignore_keys:
-            setattr(place_obj, key, value)
-    place_obj.save()
-    return jsonify(place_obj.to_dict())
+    return make_response(jsonify(new_place.to_dict()), 201)
